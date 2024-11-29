8afa9435341a2dc8373e4f0b6528be830042c005
Fossombrome <fossombrome@gmail.com>
2024-11-29 17:00:12 +0100

    rebuilded the _site

---
+++ _site/java/programmierumgebung/install-jetbrains.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo Preparing installer...
+rm -rf /tmp/luna-runtime-installer
+mkdir /tmp/luna-runtime-installer
+cd /tmp/luna-runtime-installer || exit
+
+echo "Downloading Jetbrains Toolbox for $(uname -m)..."
+if [ "$(uname -m)" == "arm64" ]; then
+  curl -L -o jetbrains-toolbox.dmg https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.5.2.35332-arm64.dmg
+else
+  curl -L -o jetbrains-toolbox.dmg https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.5.2.35332.dmg
+fi
+echo "Download Finished!"
+
+echo "Installing... (This may take some time)"
+hdiutil attach jetbrains-toolbox.dmg
+ditto /Volumes/JetBrains\ Toolbox/JetBrains\ Toolbox.app "$HOME"/Applications/JetBrains\ Toolbox.app
+echo "Installation Finished!"
+
+echo "Cleaning up..."
+hdiutil detach /Volumes/JetBrains\ Toolbox
+rm -rf /tmp/luna-runtime-installer
+echo "Done!"
